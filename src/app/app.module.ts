2fa290fd406079fb819d7c04ec913f5502ea336e
mchilders14 <childersmatt4@gmail.com>
2021-08-06 09:37:36 -0400

    ng add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'; // Adding this
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeyThereComponent } from './feature/hey-there/hey-there.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeyThereComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule // Adding this
   ],
   providers: [],
   bootstrap: [AppComponent]
